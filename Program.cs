75157a28cda7b229064edf773c3411c620ebd092
Arsentiy <ARS-9508@mail.ru>
2023-12-14 16:18:37 +0300

    Create FinalArray

--- Program.cs
+++ Program.cs
@@ -1,14 +1,14 @@
-﻿System.Console.WriteLine($"Введите значения через запятую в одну строчку для использования в массиве: ");
+﻿System.Console.Write($"Введите значения через запятую в одну строчку для использования в массиве: ");
 
 string word = Console.ReadLine();
 
-string[] CreateArray()
+string[] CreateArray()      // Создаем массив из введенный элементов.
 {
     string[] arrayAll = word.Split(",");
     return arrayAll;
 }
 
-void PrintArray(string[] array)
+void PrintArray(string[] array)  // Метод для вывода массива (Используется для финального массива.).
 {
     for (int i = 0; i < array.Length; i++)
     {
@@ -17,12 +17,34 @@ void PrintArray(string[] array)
     }
 }
 
-string[] FinalArray()
+string[] FinalArray()  // Метод для создания массива могласно условиям.
 {
-    
+    string[] array = CreateArray();
+    string Fstring = String.Empty;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            Fstring = Fstring + array[i] + ",";
+        }
+    }
+    Fstring = Fstring.Remove(Fstring.Length-1); // Для удаления запяитой в конце.
+    string[] NewArray = Fstring.Split(",");
+    return NewArray;
 }
 
 
 string[] array = CreateArray();
-PrintArray(array);
+System.Console.WriteLine($"Исходный массив: [{string.Join(", ", array)}]"); // Исходный массив.
+System.Console.WriteLine();
+System.Console.WriteLine($"Массив согласно условиям: ");
+PrintArray(FinalArray()); // Финальнй массив.
+
+
+
+
+
+
+
+
 
