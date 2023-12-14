ffc7ce010b578b7cd640c84ca2d9e6131ed42e3d
Arsentiy <ARS-9508@mail.ru>
2023-12-14 13:48:30 +0300

    Create New Vvod, CreateArray, PrintArray

--- Program.cs
+++ Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿System.Console.WriteLine($"Введите значения через запятую в одну строчку для использования в массиве: ");
+
+string word = Console.ReadLine();
+
+string[] CreateArray()
+{
+    string[] arrayAll = word.Split(",");
+    return arrayAll;
+}
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]);
+        Console.Write(", ");
+    }
+}
+
+string[] FinalArray()
+{
+    
+}
+
+
+string[] array = CreateArray();
+PrintArray(array);
+
